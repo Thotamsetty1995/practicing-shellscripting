b305803ebac846cba64c133f536df013b8e58df3
karthiksetty <karthiksetty6666@gmail.com>
2024-04-21 17:37:38 +0530

    added some info in varibles file

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-dnf install mysql -y
+# Install the MySQL repository package
+sudo dnf install -y https://dev.mysql.com/get/mysql80-community-release-el8-1.noarch.rpm
+
+# Install MySQL server and client
+sudo dnf install -y mysql-community-server mysql-community-client
+
+# Start the MySQL service
+sudo systemctl start mysqld
+
+# Secure MySQL installation (optional)
+sudo mysql_secure_installation
