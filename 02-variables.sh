ec16a7930fcb74c47d88b05a968f20330195e96e
karthiksetty <karthiksetty6666@gmail.com>
2024-04-21 15:37:23 +0530

    added some info in varibles file

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+ 
+# variable means, variable holds some value.
+
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, Im good"
+echo "Ramesh:: had your breakfast?"
+echo "Suresh:: Yes, just now I had, what about you Ramesh?" 
