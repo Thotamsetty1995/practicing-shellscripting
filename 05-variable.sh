ab4604ba25dfceec8f6b429dbb564434432dcb7e
karthiksetty <karthiksetty6666@gmail.com>
2024-04-21 16:10:03 +0530

    added some info in varibles file

---
+++ 05-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# to add variable for confidential stuffs
+
+echo "please enter your username"
+read USERNAME #here USERNAME is variable
+echo "please enter your password"
+read PASSWORD
+echo "username is: $USERNAME, Password is: $PASSWORD"
+
+
